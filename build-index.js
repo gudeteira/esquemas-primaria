9ca7b5f232b20700dbeac89ea6e6ff0b84bc5c27
pedro.gude <gudeteira@gmail.com>
2021-03-07 00:33:17 +0100

    feat: Añadir markmap-lib para crear os esquemas directamente co md\ Usar Transformer (markmap-lib) para crear directamente os esquemas desde o md directamente para  evitar cargar o iframe e poder colapsar os esquemas

--- build-index.js
+++ build-index.js
@@ -1,95 +1,108 @@
-const HTMLParser = require('node-html-parser');
-const fs = require('fs');
+const HTMLParser = require('node-html-parser')
+const { promises: fs } = require("fs");
+const rootFolder = './src/'
 
-const parseHTML = HTMLParser.parse;
-
-
-const files = process.argv.slice(2)
-let data = {}
-files.forEach(file => {
-    const dirs = file.split('/').map(d => d.split('-').join(' ').trim())
-    const filename = dirs.pop()
-    const name = filename.slice(0, filename.indexOf('.'))
-    const parent = dirs[0]
-    const child = dirs[1]
-
-    if (!data[parent] || !data[parent][child]) {
-        data = {
-            ...data,
-            [parent]: {
-                ...data[parent],
-                [child]: [{
-                    file,
-                    name,
-                    tags: dirs
-                }]
-            }
-        }
-    } else {
-        data = {
-            ...data,
-            [parent]: {
-                ...data[parent],
-                [child]: [...data[parent][child],
-                {
-                    file,
-                    name,
-                    tags: dirs
-                }
-                ]
-            }
-        }
+async function main() {
+    const parseHTML = HTMLParser.parse
+    const files = await getFiles(rootFolder, /\.md*$/)
+    const index = await fs.readFile(__dirname + '/index.html')
+    const root = parseHTML(index.toString(), {
+        script: true
+    });
+    const esquemas = root.querySelector('#esquemas')
+    esquemas.set_content('')
+    for (const file of files) {
+        const md = await fs.readFile(__dirname + '/' + file.path)
+        esquemas.appendChild(`<pre data-mm="${file.path}"> ${md.toString()}</pre>\n`)
     }
 
-});
+    root.querySelector('#menu').set_content(buildTemplate(getTree(files), rootFolder))
 
-
-const root = parseHTML(fs.readFileSync(__dirname + '/index.html').toString(), {
-    script: true
-});
-
-const a = Object.keys(data).map( id => {
-    return`<p class="menu-label is-uppercase has-text-weight-bold">${id}</p>
-
-    ${Object.keys(data[id]).map(child => {
-
-       return `<ul class="menu-list"><li><a class="is-capitalized">${child}</a> <li>
-            <ul>
-                ${data[id][child].map(node => {
-                    return `<li><a class="is-capitalized" href="${node.file}.html"> ${node.name}</a></li>`
-                    
-                }).join('')}
-            </ul>
-        </ul>`
-    }).join('')}
-    `
-}).join('')
-
-
-let html = '<ul>' + a + '</ul>'
-
-
-// const html = data.map(d => buildTemplate(d))
-root.querySelector('#menu').set_content(html)
-
-fs.writeFile('index.html', root.toString(), 'utf8', function (err) {
-    if (err) return console.log(err);
-});
-
-
-
-
-
-function buildTemplate({ file, name, tags }) {
-    return `
-    <div class="tile is-child box notification is-primary is-rounded">
-        <p class="title">
-            <a href="./${file}.html">${name}</a>
-        </p>
-        <div class="tags">
-        ${tags.map(tag => `<span class="tag is-rounded is-info">${tag}</span>`).join('')}
-        </div>    
-    </div>    
-    `
+    await fs.writeFile('index.html', root.toString(), 'utf8')
+    console.log('index.html actualizado')
 }
+
+function buildTemplate(data = {}) {
+    return '<ul>' + Object.keys(data).map(id => {
+        return `<p class="menu-label is-uppercase has-text-weight-bold">${id}</p>
+    
+        ${Object.keys(data[id]).map(child => {
+
+            return `<ul class="menu-list"><li><a class="is-capitalized">${child}</a> <li>
+                <ul>
+                    ${data[id][child].map(node => {
+                return `<li><a class="is-capitalized" data-rel-mm="${node.file.path}" href="${node.file.path}"> ${node.name}</a></li>`
+
+            }).join('')}
+                </ul>
+            </ul>`
+        }).join('')}
+        `
+    }).join('') + '</ul>'
+}
+
+
+function getTree(files = [], ignorePath = './src/') {
+    let data = {}
+    files.forEach(file => {
+        const dirs = file.path.replace(ignorePath, '').split('/').map(d => d.split('-').join(' ').trim())
+        const filename = file.name
+        const name = filename.slice(0, filename.indexOf('.')).split('-').join(' ')
+        const parent = dirs[0]
+        const child = dirs[1]
+
+        if (!data[parent] || !data[parent][child]) {
+            data = {
+                ...data,
+                [parent]: {
+                    ...data[parent],
+                    [child]: [{
+                        file,
+                        name,
+                        tags: dirs
+                    }]
+                }
+            }
+        } else {
+            data = {
+                ...data,
+                [parent]: {
+                    ...data[parent],
+                    [child]: [...data[parent][child],
+                    {
+                        file,
+                        name,
+                        tags: dirs
+                    }
+                    ]
+                }
+            }
+        }
+
+    });
+    return data
+}
+
+
+async function getFiles(path = "./src/", filter = /\.html*$/) {
+
+    const entries = await fs.readdir(path, { withFileTypes: true })
+
+    const files = entries
+        .filter(file => !file.isDirectory() && filter.test(file.name))
+        .map(file => ({ ...file, path: path + file.name }))
+
+
+    const folders = entries.filter(folder => folder.isDirectory())
+
+
+    for (const folder of folders) {
+        files.push(...await getFiles(`${path}${folder.name}/`, filter))
+    }
+
+    return files
+
+}
+
+main()
 
